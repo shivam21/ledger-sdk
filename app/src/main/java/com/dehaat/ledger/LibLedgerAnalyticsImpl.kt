1b4ccbf753b4bcec98bca0d4db6c139c0ad81911
Shivam Bhusri <shivambhusri12@gmail.com>
2023-01-10 14:20:45 +0530

    Add implmentation in app module

---
+++ app/src/main/java/com/dehaat/ledger/LibLedgerAnalyticsImpl.kt
@@ -0,0 +1,12 @@
+package com.dehaat.ledger
+
+import lib.dehaat.ledger.presentation.LibLedgerAnalytics
+
+class LibLedgerAnalyticsImpl : LibLedgerAnalytics {
+	override fun onABSWidgetViewed() {
+
+	}
+
+	override fun onABSViewDetailsClicked() {
+	}
+}
